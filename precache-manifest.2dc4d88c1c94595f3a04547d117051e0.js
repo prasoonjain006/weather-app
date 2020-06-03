8706fad922dcd1da3b1e2797e405f1692648304f
prasoonjain006 <prasoonjain006@gmail.com>
2020-06-03 15:41:10 +0530

    Updates

---
+++ precache-manifest.2dc4d88c1c94595f3a04547d117051e0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb14b03b19c314c4c26e9523059268e8",
+    "url": "/prasoonjain006/weather-app/index.html"
+  },
+  {
+    "revision": "c186f34fc832b656f9fc",
+    "url": "/prasoonjain006/weather-app/static/css/2.f0668f01.chunk.css"
+  },
+  {
+    "revision": "4f2dc9b4f0b5d622955c",
+    "url": "/prasoonjain006/weather-app/static/css/main.4cc9e8e8.chunk.css"
+  },
+  {
+    "revision": "c186f34fc832b656f9fc",
+    "url": "/prasoonjain006/weather-app/static/js/2.fe539afd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prasoonjain006/weather-app/static/js/2.fe539afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2dc9b4f0b5d622955c",
+    "url": "/prasoonjain006/weather-app/static/js/main.28753201.chunk.js"
+  },
+  {
+    "revision": "6bac3f4d72c811480a9c",
+    "url": "/prasoonjain006/weather-app/static/js/runtime-main.7008568e.js"
+  },
+  {
+    "revision": "91b106a1ba416e8b5216128205cf8f40",
+    "url": "/prasoonjain006/weather-app/static/media/bgImage.91b106a1.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/prasoonjain006/weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/prasoonjain006/weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/prasoonjain006/weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/prasoonjain006/weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/prasoonjain006/weather-app/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/prasoonjain006/weather-App/precache-manifest.55076f3f329c4b40be02dbe48e314c3b.js"
+  "/prasoonjain006/weather-app/precache-manifest.2dc4d88c1c94595f3a04547d117051e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/prasoonjain006/weather-App/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/prasoonjain006/weather-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
